85d65c7825e414b90ea75377633fbfc872598aea
Durgesh Chandra Mishra <durgesh.dev@tarte.com>
2023-06-29 12:09:03 +0530

    Handeld exceptions

--- application.py
+++ application.py
@@ -10,38 +10,43 @@ def home_page():
 
 @app.route('/predict_datapoint',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method == 'GET' :
-        return render_template('form.html')
-    else :
-        formData = {
-            'carat'   : float(request.form.get('carat')),
-            'cut'     : str(request.form.get('cut')),
-            'color'   : str(request.form.get('color')),
-            'clarity' : str(request.form.get('clarity')),
-            'depth'   : float(request.form.get('depth')),
-            'table'   : float(request.form.get('table')),
-            'x'       : float(request.form.get('x')),
-            'y'       : float(request.form.get('y')),
-            'z'       : request.form.get('z')
-        }
-        data = CustomData(
-            carat   = formData['carat'],
-            cut     = formData['cut'],
-            color   = formData['color'],
-            clarity = formData['clarity'],
-            depth   = formData['depth'],
-            table   = formData['table'],
-            x       = formData['x'],
-            y       = formData['y'],
-            z       = formData['z']
+    try :
+        if request.method == 'GET' :
+            return render_template('form.html')
+        else :
+            formData = {
+                'carat'   : float(request.form.get('carat')),
+                'cut'     : str(request.form.get('cut')),
+                'color'   : str(request.form.get('color')),
+                'clarity' : str(request.form.get('clarity')),
+                'depth'   : float(request.form.get('depth')),
+                'table'   : float(request.form.get('table')),
+                'x'       : float(request.form.get('x')),
+                'y'       : float(request.form.get('y')),
+                'z'       : request.form.get('z')
+            }
+            data = CustomData(
+                carat   = formData['carat'],
+                cut     = formData['cut'],
+                color   = formData['color'],
+                clarity = formData['clarity'],
+                depth   = formData['depth'],
+                table   = formData['table'],
+                x       = formData['x'],
+                y       = formData['y'],
+                z       = formData['z']
 
-        )
-        final_new_data = data.get_data_as_dataframe()
-        predict_pipeline = PredictPipeline()
-        predicted= predict_pipeline.predict(final_new_data)
-        predicted_value  =  round(predicted[0],2)
+            )
+            final_new_data = data.get_data_as_dataframe()
+            predict_pipeline = PredictPipeline()
+            predicted= predict_pipeline.predict(final_new_data)
+            predicted_value  =  round(predicted[0],2)
 
-        return render_template('form.html',final_result = predicted_value)
+            return render_template('form.html',final_result = predicted_value)
+    
+    except Exception as e:
+        return render_template('form.html',final_result = e)
+        
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,   debug=True)
